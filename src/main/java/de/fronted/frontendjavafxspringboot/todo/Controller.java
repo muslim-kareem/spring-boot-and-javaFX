54d8c7890d0753d928d49f39e085e1694864f2ec
Muslim Kareem <muslimkareem@icloud.com>
2023-06-21 14:22:40 +0200

    Small fix remove if statement

--- src/main/java/de/fronted/frontendjavafxspringboot/todo/Controller.java
+++ src/main/java/de/fronted/frontendjavafxspringboot/todo/Controller.java
@@ -89,14 +89,12 @@ public class Controller implements Initializable {
     }
 
     public void deleteTodoById() {
-        var selectionModel = todoListView.getSelectionModel();
-        if (selectionModel != null) {
-            Todo selectedTodo = selectionModel.getSelectedItem();
-            if(selectedTodo.getId() != null){
-                this.todoService.deleteTodoById(selectedTodo.getId());
-            }
-            // to remove the item from Frontendlist
-            setSortedTodos(selectedStatus);
+       var selectionModel = todoListView.getSelectionModel();
+        Todo selectedTodo = selectionModel.getSelectedItem();
+        if(selectedTodo != null){
+            this.todoService.deleteTodoById(selectedTodo.getId());
         }
+        // to remove the item from Frontendlist
+        setSortedTodos(selectedStatus);
     }
 }
