e574fa26817b2eeaebe960f547778e71ae36479e
Muslim Kareem <muslimkareem@icloud.com>
2023-06-26 09:30:08 +0200

    Feat setStatusAsString

--- src/main/java/de/fronted/frontendjavafxspringboot/todo/Todo.java
+++ src/main/java/de/fronted/frontendjavafxspringboot/todo/Todo.java
@@ -12,4 +12,15 @@ public class Todo {
     private String id;
     private String description;
     private TodoStatus status;
+
+    public void setStatusAsString(String status ){
+        if(status.equals(TodoStatus.OPEN.toString())){
+            this.setStatus(TodoStatus.OPEN);
+        } else if (status.equals(TodoStatus.DONE.toString())){
+            this.setStatus(TodoStatus.DONE);
+        }else {
+            this.setStatus(TodoStatus.IN_PROGRESS);
+        }
+
+    }
 }
